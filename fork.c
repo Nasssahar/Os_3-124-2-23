598d355158e9a1bef0398a8deb2eeb5e2d862659
Haritonova <nastyahar05@gmail.com>
2024-04-19 11:14:40 +0700

    Добавили fork.c

--- fork.c
+++ fork.c
@@ -6,11 +6,26 @@ int main() {
            pid_t procid, parentid, result;
            int  a=0;
            result = fork();
-           a=a=1;
-           procid = getpid();
-           parentid = getppid();
-           printf("Ид. текущего процесса: %d\n", procid);
-           printf("Ид. родительского процесса: %d\n", parentid);
-           printf("значение переменной a:%d\n", a);
+           if(result<0) {
+                   printf("Ошибка при выполнении fork()\n");
+                   exit(-1);
+           }
+           else if(result==00) {
+                   a=a=1;
+                   procid = getpid();
+                   parentid = getppid();
+                   printf("Ид. текущего процесса: %d\n", procid);
+                   printf("Ид. родительского процесса: %d\n", parentid);
+                   printf("значение переменной a:%d\n", a);
+           }
+           else {
+                   a=a+1;
+                   procid = getpid();
+                   parentid = getppid();
+                   printf("Ид. текущего процесса: %d\n", procid);
+                   printf("Ид. родительского процесса: %d\n", parentid);
+                   printf("значение переменной a:%d\n", a);
+                   printf("значение переменной result: %d\n", result);
+           }
            return 0;
-}
+} 
