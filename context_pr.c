96ca2a0bf2fae237b10bc31b14f4f90c71f4d8a9
Haritonova <nastyahar05@gmail.com>
2024-04-26 11:25:52 +0700

    добавили запуск программы в текущем каталоге для дочернего процесса

--- context_pr.c
+++ context_pr.c
@@ -3,9 +3,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[], char *envp[]) {
-        int result;
-        result = execle("/bin/cat", "/bin/cat", "args.c", 0, envp);
-        printf("ошибка при выполнении системного вызова exec()\n");
-        exit(-1);
-        return 0;
+        pid_t procid, parentid, result;
+        printf("Порождаем вечерний процесс\n");
+        result = fork();
+        if(result<0) {
+                printf("ошибка при выполнении fork()\n");
+                exit(-1);
+        }
+        else if(result==0) {
+                procid = getpid();
+                parentid = getppid();
+                printf("Ид. текущего процесса: %d\n", procid);
+                printf("Ид. родительского процесса: %d\n", parentid);
+                printf("значение переменной result: %d\n", result);
+                execle("./args.out", "./args.out", "args.c", 0, envp);
+                printf("ошибка при выполнении системного вызова exec()\n");
+                exit(-1);
+        }
+        else {
+                 procid = getpid();
+                 parentid = getppid();
+                 printf("Ид. текущего процесса: %d\n", procid);
+                 printf("Ид. родительского процесса: %d\n", parentid);
+                 printf("значение переменной result: %d\n", result);
+       }
+       return 0;
 }
