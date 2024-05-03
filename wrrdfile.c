d1ca30c8ef4026868537eaa2c70b7f9729f42357
Haritonova <nastyahar05@gmail.com>
2024-05-03 11:21:10 +0700

    Dobavili chtenie iz faila wrfile.txt

--- wrrdfile.c
+++ wrrdfile.c
@@ -7,6 +7,7 @@ int main() {
         int fd;
         size_t size;
         char string[] = "Gr.3.124.2.23";
+        //Zapis v fail
         if ((fd = open("wrfile.txt", O_WRONLY|O_CREAT, 0666))<0) {
                 printf(" Ne ydalos otkrit fail na zapis\n");
                 exit(-1);
@@ -16,7 +17,24 @@ int main() {
                  printf("Ne ydalos zapisat v fail strokt iz 14 bait\n");
                  exit(-1);
         }
-        ptint("stroka zapisana v fail wrfile.txt\n");
+        printf("stroka zapisana v fail wrfile.txt\n");
+        if (close(fd) < 0) {
+                printf ("ne ydalos zakrit fail\n");
+                exit(-1);
+        }
+        //Chteni iz faila
+        char resstring[14];
+         if ((fd = open("wrfile.txt", O_RDONLY))<0) {
+                printf(" Ne ydalos otkrit fail na chtenia\n");
+                exit(-1);
+        }
+        size = read(fd, resstring, 14);
+        if (size != 14) {
+                 printf("Ne ydalos zapisat v fail iz 14 bait\n");
+                 exit(-1);
+        }
+        printf("stroka prochitana iz fail wrfile.txt\n");
+        printf("%s\n", resstring);
         if (close(fd) < 0) {
                 printf ("ne ydalos zakrit fail\n");
                 exit(-1);
