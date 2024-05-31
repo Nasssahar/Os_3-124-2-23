08b87ac541ae2042fe90de3fec38f67b64d21222
Haritonova <nastyahar05@gmail.com>
2024-05-31 11:09:11 +0700

    Dobavili dve programm, rabotaychie c semaforom

---
+++ sems2.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+        int semid;
+        char pathname[] = "for_ipc.ipc";
+        key_t key;
+        struct sembuf mybuf;
+        if((key = ftok(pathname,0)) < 0) {
+                printf("Ne ydalos cgenerirovat klych ipc\n");
+                exit(-1);
+        }
+        if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+            printf("Ne ydalos polychit deckriptor macciva cemaforov\n");
+            exit(-1);
+        }
+        mybuf.sem_op = 1;
+        mybuf.sem_flg = 0;
+        mybuf.sem_num = 0;
+        printf("Razblokirovka prosess, zapychenogo ranee a\n");
+        if(semop(semid, &mybuf, 1) < 0) {
+                printf("ne ydalos vipolniot operachiy nad semaforom\n");
+                exit(-1);
+        }
+        printf("Znachenie semafora ystanovleno v edinichy\n");
+        return 0;
+}
+
+
+
+
