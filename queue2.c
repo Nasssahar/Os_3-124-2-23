00d2fccf862ab845585ea2da633b6c41c3637214
Haritonova <nastyahar05@gmail.com>
2024-06-07 11:53:23 +0700

    Dobavili programm rabot c ochered coobcheniy

---
+++ queue2.c
@@ -0,0 +1,38 @@
+#include  <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define LAST_MESSAGE 255
+int main() {
+        int msqid;
+        char pathname[] = "for_ipc.ipc";
+        key_t key;
+        int len, maxlen;
+        struct mymsgbuf {
+                long mtype;
+                char mtext[81];
+        } mybuf;
+        if((key = ftok(pathname, 0)) < 0) {
+                printf("Ne ydalos cgenerirovat key ipc\n");
+                exit(-1);
+        }
+        if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
+                printf("Ne ydalos cozdat ochered ili polychit k ney dostyp\n");
+                exit(-1);
+        }
+        while(1) {
+                maxlen = 81;
+               if (msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 0, 0) < 0) {
+                       printf("Ne ydalos polychit  coobchenie v ochered\n");
+                       exit(-1);
+               }
+               if (mybuf.mtype == LAST_MESSAGE) {
+                       msgctl(msqid, IPC_RMID, (struct  msqid_ds *)NULL);
+                       exit(0);
+               }
+               printf("Tip  coobchenie: %ld, \inform chst: %s\n", mybuf.mtype, mybuf.mtext);
+         }
+         return 0;
+}
